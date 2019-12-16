d99b0aabc34566bb64979206750b610d5d776988
Tobias <tobiasdeyle@me.com>
2019-12-16 13:52:04 +0100

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//Manager in Project settings
+//Script Execution Order Irgendeinen negativen Wert vor die Default time.
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    [SerializeField] private List<ManagerModule> _modules;
+
+    public void Register(ManagerModule module)
+    {
+        _modules.Add(module);
+    }
+
+    public void Unregister(ManagerModule module)
+    {
+        _modules.Remove(module);
+    }
+
+    public T Get<T>() where T : ManagerModule
+    {
+        return _modules.OfType<T>().First();
+    }
+}
---
+++ Assets/Scripts/ManagerModule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerModule : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManager.Instance.Register(this);
+    }
+}
