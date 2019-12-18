878f25880259938b8999d0ffe7cae3a7019a072f
Tobias <tobiasdeyle@me.com>
2019-12-18 12:07:46 +0100

    Sachen und Dinge 2

--- Assets/Scripts/Button/Button.cs
+++ Assets/Scripts/Button/Button.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public Door door;
+    private bool _gotPressed;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed)
         {
+            _gotPressed = true;
             door.OpenDoor();
+            transform.position += new Vector3(0, -0.1f, 0);
         }
     }
 }
