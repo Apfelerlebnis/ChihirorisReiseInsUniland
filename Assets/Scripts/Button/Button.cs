584ef00a93f76433b0c5329e6b9f4348926dc0c1
Jennifer <jebethke@stud.mediadesign.de>
2020-01-27 16:27:58 +0100

    bush fix and button stuff

--- Assets/Scripts/Button/Button.cs
+++ Assets/Scripts/Button/Button.cs
@@ -6,14 +6,27 @@ public class Button : MonoBehaviour
 {
     public Door door;
     private bool _gotPressed;
+    public int GeisterNeeded;
+    public bool ObjectNeeded;
 
     void OnTriggerEnter(Collider other)
     {
-        if ((other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed) || (other.gameObject.layer == LayerMask.NameToLayer("MoveableObject") && !_gotPressed))
+        /*if (GeisterNeeded  || (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed))
         {
             _gotPressed = true;
             door.OpenDoor();
             transform.position += new Vector3(0, -0.1f, 0);
+        }*/
+
+        if(ObjectNeeded == true && (other.gameObject.layer == LayerMask.NameToLayer("MoveableObject") && !_gotPressed))
+        {
+
+            _gotPressed = true;
+            door.OpenDoor();
+            transform.position += new Vector3(0, -0.1f, 0);
+
+
         }
+
     }
 }
