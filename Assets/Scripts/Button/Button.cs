794ceb901221bfb9a24e46472c72f4e08962228f
Jennifer <jebethke@stud.mediadesign.de>
2020-01-27 17:09:00 +0100

    can choose to activate button with object and/or a number of viecher

--- Assets/Scripts/Button/Button.cs
+++ Assets/Scripts/Button/Button.cs
@@ -8,17 +8,13 @@ public class Button : MonoBehaviour
     private bool _gotPressed;
     public int GeisterNeeded;
     public bool ObjectNeeded;
+    public PlayerManagerModule playerManagerModule;
 
     void OnTriggerEnter(Collider other)
     {
-        /*if (GeisterNeeded  || (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed))
-        {
-            _gotPressed = true;
-            door.OpenDoor();
-            transform.position += new Vector3(0, -0.1f, 0);
-        }*/
 
-        if(ObjectNeeded == true && (other.gameObject.layer == LayerMask.NameToLayer("MoveableObject") && !_gotPressed))
+        if (ObjectNeeded == true && (other.gameObject.layer == LayerMask.NameToLayer("MoveableObject") && !_gotPressed) || 
+            GeisterNeeded <= playerManagerModule.GetComponent<PlayerManagerModule>().dudes.Count && (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed))
         {
 
             _gotPressed = true;
@@ -27,6 +23,8 @@ public class Button : MonoBehaviour
 
 
         }
+        else
+            return;
 
     }
 }
