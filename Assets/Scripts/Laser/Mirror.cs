b64810d6ab6d82a08e048122b141b28b713c1c8f
Jennifer <jebethke@stud.mediadesign.de>
2020-02-03 11:31:52 +0100

    mirror created, can be pushed along one axis with a customizable amount of dudes, started the light source

---
+++ Assets/Scripts/Laser/Mirror.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    public int GeisterNeeded;
+    public PlayerManagerModule Player;
+    public bool xAchseMoveable;
+    public bool zAchseMoveable;
+
+    private void Update()
+    {
+        if(GeisterNeeded == Player.GetComponent<PlayerManagerModule>().dudes.Count && xAchseMoveable == true)
+        {
+
+            GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionX & ~RigidbodyConstraints.FreezeRotationZ;
+
+        }
+
+        if (GeisterNeeded == Player.GetComponent<PlayerManagerModule>().dudes.Count && zAchseMoveable == true)
+        {
+
+            GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionZ & ~RigidbodyConstraints.FreezeRotationZ;
+
+        }
+    }
+}
+
