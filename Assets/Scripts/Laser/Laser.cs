e3619b76e68cfa03cce728476acd47daa09e43f1
Jennifer <jebethke@stud.mediadesign.de>
2020-02-04 14:14:35 +0100

    lichtquelle + laser script finished

--- Assets/Scripts/Laser/Laser.cs
+++ Assets/Scripts/Laser/Laser.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Laser : MonoBehaviour
 {
+    public float xAchse;
+    public float zAchse;
     RaycastHit hit;
-
+    int layerMask = 1 << 8;
     private void Update()
     {
         GetComponent<LineRenderer>().SetPosition(0, transform.position);
-        Physics.Raycast(transform.position, new Vector3(transform.position.x * 100, transform.position.y, transform.position.z), out hit , 1000);
+        Physics.Raycast(transform.position, new Vector3(transform.position.x + xAchse, transform.position.y, transform.position.z + zAchse), out hit , 1000, ~layerMask);
 
 
         if (hit.collider)
