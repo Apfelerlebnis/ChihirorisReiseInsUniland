f5f7988452d4827b9503298aa0f9e499e9b0468b
Jennifer <jebethke@stud.mediadesign.de>
2020-02-04 09:38:31 +0100

    created lightsource, laser is a WIP

--- Assets/Scripts/Laser/Laser.cs
+++ Assets/Scripts/Laser/Laser.cs
@@ -1,23 +1,28 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Laser : MonoBehaviour
 {
-    LineRenderer line;
-    Vector3 lightSource = new Vector3();
-    
+    RaycastHit hit;
 
-
-
-    void Start()
+    private void Update()
     {
-        
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        Physics.Raycast(transform.position, new Vector3(transform.position.x * 100, transform.position.y, transform.position.z), out hit , 1000);
+
+
+        if (hit.collider)
+        {
+            GetComponent<LineRenderer>().SetPosition(1, hit.point);
+
+        }
+        else
+        {
+            GetComponent<LineRenderer>().SetPosition(1, new Vector3(transform.position.x * 10, transform.position.y, transform.position.z));
+        }
     }
 
-    void Update()
-    {
-        
-    }
+
 }
-*/
