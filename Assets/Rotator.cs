57d7fef689d808d23e0db7a5596d570fb2e6c0e7
Niiiiiiiiiiii <nici@bambusource.de>
2020-02-06 17:32:39 +0100

    Level 1 detailed, added assets and lights and stuff uwu

---
+++ Assets/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float SpeedY;
+    public float SpeedZ;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), SpeedY * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 1), SpeedZ * Time.deltaTime);
+    }
+
+}
