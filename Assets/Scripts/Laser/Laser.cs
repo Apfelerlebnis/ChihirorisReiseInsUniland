41cdf578f18c39aa3e44121f1a113ad5144ac812
Jennifer <civileey@gmail.com>
2020-02-11 16:50:12 +0100

    laser immernoch rip, werde morgen weiter daran arbeiten (immerhin geht der jtzt net mehr durch objekte mit collidern) >:V

--- Assets/Scripts/Laser/Laser.cs
+++ Assets/Scripts/Laser/Laser.cs
@@ -1,67 +1,71 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class Laser : MonoBehaviour
 {
-    public float xAchse;
-    public float zAchse;
     RaycastHit hit;
+
     //readonly int layerMask = 1 << 8;
     public Collider collider1;
     public Collider collider2;
     public Collider lightGoal;
     public Door door;
-    
+    private Ray ray;
 
     private void Start()
     {
-        List<Collider> colliders = new List<Collider>();
-        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(0, new Vector3(0, 0, 0));
+
     }
+
     private void Update()
     {
-
-        Mirror();
-
-    }
-
-
-    void Mirror()
-    {
-        Physics.Raycast(transform.position, transform.forward, out hit, 500, PlayerManagerModule.LevelLayerMask);
-
-        if (hit.collider)
         {
 
-            GetComponent<LineRenderer>().SetPosition(1, hit.point);
+            Mirror();
+            ray = new Ray(transform.position, transform.right);
+        }
 
-            
-            if (collider2 == hit.collider)
+
+        void Mirror()
+        {
+            Physics.Raycast(ray.origin, ray.direction, out hit, 5000, PlayerManagerModule.LevelLayerMask);
+            Debug.DrawRay(ray.origin, ray.direction);
+            if (hit.collider)
             {
-                GetComponent<LineRenderer>().positionCount = 3;
-                Vector3 pos = Vector3.Reflect(hit.point, hit.normal);
-                if(!Physics.Raycast(hit.point+ hit.normal*0.2f, hit.normal, out RaycastHit hit2, Mathf.Infinity, PlayerManagerModule.LevelLayerMask))
+
+                GetComponent<LineRenderer>().SetPosition(1, new Vector3(hit.point.x, 0, hit.point.z));
+
+
+                if (collider2 == hit.collider)
                 {
-                    hit2.point = hit.point + hit.normal * 10;
+                    GetComponent<LineRenderer>().positionCount = 3;
+                    if (!Physics.Raycast(hit.point + hit.normal * 0.2f, hit.normal, out RaycastHit hit2, Mathf.Infinity, PlayerManagerModule.LevelLayerMask))
+                    {
+                        hit2.point = hit.point + hit.normal * 10;
+                    }
+
+                    if (lightGoal == hit2.collider)
+                    {
+                        door.OpenDoor();
+                    }
+
+                    GetComponent<LineRenderer>().SetPosition(2, hit2.point);
                 }
-                if (lightGoal == hit2.collider)
+                else
                 {
-                    door.OpenDoor();
+                    GetComponent<LineRenderer>().positionCount = 2;
+
                 }
-                GetComponent<LineRenderer>().SetPosition(2, hit2.point);
             }
             else
-            {
-                GetComponent<LineRenderer>().positionCount = 2;
+                Debug.Log("Didnt hit");
 
-            }
+
+
+
         }
-        
-
-
-
     }
 }
