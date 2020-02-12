9e9a309b9cbbf7f90f0f35e22c5e0c03599ce0f3
Jennifer <jebethke@stud.mediadesign.de>
2020-02-12 15:29:42 +0100

    small fix

--- Assets/Scripts/Laser/Laser.cs
+++ Assets/Scripts/Laser/Laser.cs
@@ -8,12 +8,8 @@ public class Laser : MonoBehaviour
     RaycastHit hit;
 
     //readonly int layerMask = 1 << 8;
-    public Collider collider1;
-    public Collider collider2;
     public Collider lightGoal;
-    public Door door;
     private Ray ray;
-    private Ray ray2;
     private void Start()
     {
         GetComponent<LineRenderer>().SetPosition(0, new Vector3(0, 0, 0));
@@ -36,30 +32,13 @@ public class Laser : MonoBehaviour
             {
                 Vector3 hitPoint = transform.InverseTransformPoint(hit.point);
                 GetComponent<LineRenderer>().SetPosition(1, hitPoint);
-
-                if (collider2 == hit.collider)
+                if (hit.collider.CompareTag("Mirror"))
                 {
-                    //Vector3 reflectPos = Vector3.Reflect(ray.direction, hitPoint.normalized);
-                    GetComponent<LineRenderer>().positionCount = 3;
-                    Physics.Raycast((hitPoint - hit.point), hitPoint.normalized, out RaycastHit hit2, Mathf.Infinity, PlayerManagerModule.LevelLayerMask);
-                    Debug.DrawRay((hitPoint - hit.point), hitPoint.normalized, Color.red);
-                    Debug.Log("Alex hat einen kleinen Penis  -Kadir");
-                    hit2.point = hitPoint + hit.normal * 10;
-                        if (lightGoal == hit2.collider)
-                        {
-                            door.OpenDoor();
-                        }
-                    
-
-                    
-                    Vector3 hitPoint2 = transform.InverseTransformPoint(hit2.point);
-                    GetComponent<LineRenderer>().SetPosition(2, hitPoint2);
+                    Mirror mirror = hit.collider.GetComponent<Mirror>();
+                    mirror.CastHit();
                 }
                 else
-                {
-                    GetComponent<LineRenderer>().positionCount = 2;
-
-                }
+                    return;
             }
 
         }
--- Assets/Scripts/Laser/Mirror.cs
+++ Assets/Scripts/Laser/Mirror.cs
@@ -7,7 +7,9 @@ public class Mirror : MonoBehaviour
     public PlayerManagerModule Player;
     public bool xAchseMoveable;
     public bool zAchseMoveable;
-
+    public Door door;
+    public GameObject lightGoal;
+    private Ray ray;
 
     private void Update()
     {
@@ -15,7 +17,6 @@ public class Mirror : MonoBehaviour
         {
 
             GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionX & ~RigidbodyConstraints.FreezeRotationZ;
-
         }
 
         if (GeisterNeeded == Player.GetComponent<PlayerManagerModule>().dudes.Count && zAchseMoveable == true)
@@ -26,10 +27,25 @@ public class Mirror : MonoBehaviour
         }
 
 
-
-
     }
+    public void CastHit()
+    {
+          ray = new Ray(transform.position, transform.forward);
+          Physics.Raycast(ray.origin, new Vector3(0,0,1), out RaycastHit hit, 5000, PlayerManagerModule.LevelLayerMask);
+          if (hit.collider)
+          {
+              Vector3 hitPoint2 = transform.InverseTransformPoint(hit.point);
+              GetComponent<LineRenderer>().SetPosition(1, hitPoint2);
+              if (lightGoal == hit.collider)
+              {
+                  door.OpenDoor();
+              }
 
+          }
+
+       
+    }
+  
 
 }
 
