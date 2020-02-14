492b898018b4f76259427f14af387c4a63e532c4
Niiiiiiiiiiii <nici@bambusource.de>
2020-02-14 15:52:09 +0100

    FANCY BUTTON!

--- Assets/Scripts/Button/Button.cs
+++ Assets/Scripts/Button/Button.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour
 {
@@ -12,20 +13,62 @@ public class Button : MonoBehaviour
     public PlayerManagerModule playerManagerModule;
     public Light light;
     public GameObject particles;
+    public Image GeisterFillImage;
+
+    public bool bDoorOpened = false;
+    public float LerpRate = 1.0f;
+    float LerpTimer = 0.0f;
+    float fFillamount = 0.0f;
 
     void OnTriggerEnter(Collider other)
-    {
+    {   
+        if (other.gameObject.tag == "Player")
+        {
+            fFillamount = (float)playerManagerModule.GetComponent<PlayerManagerModule>().dudes.Count / (float)GeisterNeed;
+            LerpTimer = 0.0f;
+        }
+        //Debug.Log(playerManagerModule.GetComponent<PlayerManagerModule>().dudes.Count);
+        
+
         if (ObjectNeeded == true && (other.gameObject.layer == LayerMask.NameToLayer("MoveableObject") && !_gotPressed) || 
             GeisterNeed <= playerManagerModule.GetComponent<PlayerManagerModule>().dudes.Count && (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_gotPressed && GeisterNeeded == true))
         {
+            
 
             _gotPressed = true;
             door.OpenDoor();
             particles.gameObject.SetActive(true);
-            light.intensity = 0.5f;
+            //light.intensity = 0.5f;
         }
         else
             return;
 
+    }
+
+    void Update()
+    {
+
+        Debug.Log(LerpTimer);
+        if (GeisterFillImage.fillAmount != fFillamount)
+        {
+            LerpTimer += Time.deltaTime * LerpRate;
+
+            GeisterFillImage.fillAmount = Mathf.Lerp(GeisterFillImage.fillAmount, fFillamount, LerpTimer);
+
+            if (light.intensity <= .85f)
+                light.intensity = Mathf.Lerp(GeisterFillImage.fillAmount, fFillamount, LerpTimer);
+        } else
+        {
+            LerpTimer = 0.0f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            LerpTimer = 0.0f;
+            fFillamount = 0;
+        }
     }
 }
