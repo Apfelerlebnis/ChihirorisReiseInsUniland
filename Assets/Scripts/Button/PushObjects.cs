fe55c155d5444a92cae8210e2be09ffa1201fa7f
Jennifer <jebethke@stud.mediadesign.de>
2020-02-17 15:46:50 +0100

    moveable obj rework

--- Assets/Scripts/Button/PushObjects.cs
+++ Assets/Scripts/Button/PushObjects.cs
@@ -6,8 +6,6 @@ public class PushObjects : MonoBehaviour
     //put on object that needs to be pushed
     //also add a rigid body component and freeze position (y) and freeze rotation (x and z)
 {
-
-    public int pushPower = 100;
     public int GeisterNeeded;
     public PlayerManagerModule Player;
     public float Drag;
@@ -18,14 +16,14 @@ public class PushObjects : MonoBehaviour
     {
         if (GeisterNeeded == Player.GetComponent<PlayerManagerModule>().dudes.Count)
         {
-            this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionZ & ~RigidbodyConstraints.FreezeRotationY & ~RigidbodyConstraints.FreezePositionX;
+            this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionZ & ~RigidbodyConstraints.FreezePositionX;
             this.GetComponent<Rigidbody>().drag = 20 * Drag;
             this.GetComponent<Rigidbody>().angularDrag = 200 * AngularDrag;
         }
 
         if (GeisterNeeded < Player.GetComponent<PlayerManagerModule>().dudes.Count)
         {
-            this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionZ & ~RigidbodyConstraints.FreezeRotationY & ~RigidbodyConstraints.FreezePositionX;
+            this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionZ & ~RigidbodyConstraints.FreezePositionX;
             this.GetComponent<Rigidbody>().drag = 0.5f * Drag;
             this.GetComponent<Rigidbody>().angularDrag = 5 * AngularDrag;
         }
