d6ae2dcf41451e87e1e5686c805870a68ed0b9c0
Jennifer <jebethke@stud.mediadesign.de>
2020-02-19 14:03:12 +0100

    in arbeit dudes ende

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,45 +1,39 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public UnityEngine.Animator animator;
     private int levelToLoad;
-    public List<GameObject> dudesImLevel = new List<GameObject>();
-    public PlayerManagerModule player;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-
+            
             FadeToNextLevel();
 
         }
 
     }
+
     void FadeToNextLevel()
     {
 
         FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
     void FadeToLevel(int scene)
     {
         levelToLoad = scene;
         animator.SetTrigger("Fade_Out");
 
     }
+
     void OnFadeComplete()
     {
         SceneManager.LoadScene(levelToLoad);
-
-    }
-
-    void DudesCount()
-    {
-
 
     }
 
