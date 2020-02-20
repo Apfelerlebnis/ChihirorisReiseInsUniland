c4b6c30d184f99819467088f47de46b02959832b
Jennifer <jebethke@stud.mediadesign.de>
2020-02-20 17:58:09 +0100

    audio manager and music works

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource sfxSource;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+          
+
+
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0, clips.Length)];
+
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+        
+    }
+
+}
