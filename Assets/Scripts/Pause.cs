840e569855e510abab93c56d11d63857ec1aff38
Jennifer <civileey@gmail.com>
2020-11-08 23:28:22 +0100

    added pause menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    public AudioMixer audioMixer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DontDestroyOnLoad(this);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pausing();
+            }
+        }
+        
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pausing()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void Quit()
+    {
+        Debug.Log("Quitting!");
+        Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);   
+    }
+}
