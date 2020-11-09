f4e5de7f967e4fb0a5f2bb7e44f240207120d26a
Niiiiiiiiiiii <nici@bambusource.de>
2020-11-09 15:10:25 +0100

    Added Resolution Selector

--- Assets/Scripts/SwitchRenderPipeline.cs
+++ Assets/Scripts/SwitchRenderPipeline.cs
@@ -6,14 +6,13 @@ public class SwitchRenderPipeline : MonoBehaviour
     public RenderPipelineAsset exampleAssetA;
     public RenderPipelineAsset exampleAssetB;
 
-    void Update()
-    {
-        if (Input.GetKeyDown("1"))
+    public void Toggle(bool newValue){
+        if (newValue == true)
         {
             GraphicsSettings.renderPipelineAsset = exampleAssetA;
             Debug.Log("Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
-        }
-        else if (Input.GetKeyDown("2"))
+        } 
+        else
         {
             GraphicsSettings.renderPipelineAsset = exampleAssetB;
             Debug.Log("Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
