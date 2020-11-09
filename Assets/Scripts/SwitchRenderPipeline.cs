539962435470e066b7c59707e5f0dedf28496546
Niiiiiiiiiiii <nici@bambusource.de>
2020-11-09 14:09:43 +0100

    Stuff

---
+++ Assets/Scripts/SwitchRenderPipeline.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SwitchRenderPipeline : MonoBehaviour
+{
+    public RenderPipelineAsset exampleAssetA;
+    public RenderPipelineAsset exampleAssetB;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            GraphicsSettings.renderPipelineAsset = exampleAssetA;
+            Debug.Log("Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            GraphicsSettings.renderPipelineAsset = exampleAssetB;
+            Debug.Log("Active render pipeline asset is: " + GraphicsSettings.renderPipelineAsset.name);
+        }
+    }
+}
